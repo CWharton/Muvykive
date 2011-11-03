bee5cbdd1cacbf826c02dd9cf78021c56a1ba954
Chris Wharton <chris@cwharton.com>
2011-11-03 17:31:16 -0500

    Initial Commit

---
+++ Muvykive.Repository/SessionFactory.cs
@@ -0,0 +1,49 @@
+﻿using Muvykive.Repository.SessionStorage;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace Muvykive.Repository
+{
+    public class SessionFactory
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static void Init()
+        {
+            var config = new Configuration();
+            config.AddAssembly("Muvykive.Repository");
+
+            config.Configure();
+
+            _sessionFactory = config.BuildSessionFactory();
+        }
+
+        private static ISessionFactory GetSessionFactory()
+        {
+            if (_sessionFactory == null)
+                Init();
+
+            return _sessionFactory;
+        }
+
+        private static ISession GetNewSession()
+        {
+            return GetSessionFactory().OpenSession();
+        }
+
+        public static ISession GetCurrentSession()
+        {
+            ISessionStorageContainer sessionStorageContainer = SessionStorageFactory.GetStorageContainer();
+
+            ISession currentSession = sessionStorageContainer.GetCurrentSession();
+
+            if (currentSession == null)
+            {
+                currentSession = GetNewSession();
+                sessionStorageContainer.Store(currentSession);
+            }
+
+            return currentSession;
+        }
+    }
+}
---
+++ Muvykive.Web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Muvykive.Web.UI.MvcApplication" Language="C#" %>
